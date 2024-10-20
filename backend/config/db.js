2763216a11b22f64b36553a70abc1bffbecee097
Bibash Chaudhary <bibashcdry46@gmail.com>
2024-10-20 21:55:17 +0545

    Delete backend directory
    i want to delete

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://greatstack:984562@cluster0.lajjlar.mongodb.net/food-del').then(()=>{
-      console.log("DB connected")
-    });
-
-}
